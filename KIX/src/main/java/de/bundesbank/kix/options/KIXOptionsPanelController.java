8d03286756eadc52a98e1ee149ffd552b7c128b2
Thomas Witthohn <thomas.witthohn@bundesbank.de>
2015-09-11 15:05:24 +0200

    Added OptionPanel for UnchainingMethod

---
+++ KIX/src/main/java/de/bundesbank/kix/options/KIXOptionsPanelController.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.bundesbank.kix.options;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
+
+@OptionsPanelController.SubRegistration(
+        location = "Demetra",
+        displayName = "#AdvancedOption_DisplayName_KIX",
+        keywords = "#AdvancedOption_Keywords_KIX",
+        keywordsCategory = "Demetra/KIX",
+        id = KIXOptionsPanelController.ID, position = 7)
+@org.openide.util.NbBundle.Messages({"AdvancedOption_DisplayName_KIX=KIX", "AdvancedOption_Keywords_KIX=KIX"})
+public final class KIXOptionsPanelController extends OptionsPanelController {
+
+    public static final String ID = "Demetra/KIX";
+
+    private KIXPanel panel;
+    private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+    private boolean changed;
+
+    @Override
+    public void update() {
+        getPanel().load();
+        changed = false;
+    }
+
+    @Override
+    public void applyChanges() {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                getPanel().store();
+                changed = false;
+            }
+        });
+    }
+
+    @Override
+    public void cancel() {
+    }
+
+    @Override
+    public boolean isValid() {
+        return getPanel().valid();
+    }
+
+    @Override
+    public boolean isChanged() {
+        return changed;
+    }
+
+    @Override
+    public HelpCtx getHelpCtx() {
+        return null; // new HelpCtx("...ID") if you have a help set
+    }
+
+    @Override
+    public JComponent getComponent(Lookup masterLookup) {
+        return getPanel();
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
+
+    private KIXPanel getPanel() {
+        if (panel == null) {
+            panel = new KIXPanel(this);
+        }
+        return panel;
+    }
+
+    void changed() {
+        if (!changed) {
+            changed = true;
+            pcs.firePropertyChange(OptionsPanelController.PROP_CHANGED, false, true);
+        }
+        pcs.firePropertyChange(OptionsPanelController.PROP_VALID, null, null);
+    }
+
+}
